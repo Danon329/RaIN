b2881184f9c4544cde7882f091093a613c60603c
Daniel Feldman <daniel.salit@hotmail.com>
2025-10-09 21:38:59 +0200

    Responsive Jumping

--- RaIn/scenes/player/Player.cs
+++ RaIn/scenes/player/Player.cs
@@ -5,16 +5,20 @@ namespace Game.Entity;
 public partial class Player : CharacterBody2D
 {
     [Export]
-    private float speed = 200f;
+    private float speed = 800f;
     [Export]
-    private float jump = -700f;
+    private float jump = -800f;
     [Export]
-    private float gravity = 600f;
+    private float gravity = 1200f;
 
     public override void _PhysicsProcess(double delta)
     {
         if (!IsOnFloor())
         {
+            if (Velocity.Y < 0 && !Input.IsActionPressed("jump"))
+            {
+                Velocity = Vector2.Zero;
+            }
             Velocity += new Vector2(0, gravity) * (float)delta;
         }
 
@@ -28,10 +32,13 @@ public partial class Player : CharacterBody2D
         float dir = Input.GetAxis("left", "right");
         Velocity = new Vector2(dir * speed, Velocity.Y);
 
+        // TODO: coyote time
         // Jumping
         if (Input.IsActionPressed("jump") && IsOnFloor())
         {
             Velocity = new Vector2(0, jump);
         }
+
+        // Think about Wall jump
     }
 }
